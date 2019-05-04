63a0cda99c3cff22953b2e0f3bfbafc6ae79002e
Juri Em <juriem1974@gmail.com>
2019-05-04 09:23:14 +0300

    Mark selected note in list when page reloaded

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -30,9 +30,15 @@ NoteList.propTypes = {
 };
 
 export default createContainer(() => {
+    const selectedNoteId = Session.get('selectedNoteId');
     Meteor.subscribe('notes');
     return {
-        notes: Notes.find().fetch()
+        notes: Notes.find().fetch().map(note => {
+            return {
+                ...note,
+                selected: note._id === selectedNoteId
+            }
+        })
     }
 
 }, NoteList);
--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import moment from 'moment';
 import { Session } from "meteor/session";
 import { createContainer } from 'meteor/react-meteor-data';
+
+import moment from 'moment';
 
 export const NoteListItem = (props) => {
     return (
@@ -9,6 +10,7 @@ export const NoteListItem = (props) => {
             props.Session.set('selectedNoteId', props.note._id);
         }}>
             <h5>{props.note.title || 'Untitled note'}</h5>
+            { props.note.selected ? 'selected' : undefined }
             <p>{moment(props.note.updatedAt).format(props.dateFormat)}</p>
         </div>
     )
@@ -24,6 +26,6 @@ NoteListItem.defaultProps = {
     dateFormat: 'M/DD/YY HH:mm:ss'
 };
 
-export default createContainer(()=>{
+export default createContainer(() => {
     return { Session };
 }, NoteListItem);
