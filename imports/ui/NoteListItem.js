b4cc8aae73909fb436de21922bd41f096425eed2
Juri Em <juriem1974@gmail.com>
2019-05-04 08:31:43 +0300

    Add selectedNotId session property to NoteListItem

--- imports/ui/NoteListItem.js
+++ imports/ui/NoteListItem.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import moment from 'moment';
+import { Session } from "meteor/session";
+import { createContainer } from 'meteor/react-meteor-data';
 
-const NoteListItem = (props) => {
+export const NoteListItem = (props) => {
     return (
-        <div>
+        <div onClick={() => {
+            props.Session.set('selectedNoteId', props.note._id);
+        }}>
             <h5>{props.note.title || 'Untitled note'}</h5>
             <p>{moment(props.note.updatedAt).format(props.dateFormat)}</p>
         </div>
     )
 };
 
-
-
 NoteListItem.propTypes = {
     dateFormat: React.PropTypes.string,
-    note: React.PropTypes.object.isRequired
+    note: React.PropTypes.object.isRequired,
+    Session: React.PropTypes.object.isRequired
 };
 
 NoteListItem.defaultProps = {
     dateFormat: 'M/DD/YY HH:mm:ss'
 };
 
-export default NoteListItem;
+export default createContainer(()=>{
+    return { Session };
+}, NoteListItem);
--- imports/ui/NoteListItem.test.js
+++ imports/ui/NoteListItem.test.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import expect from 'expect';
 import { mount } from 'enzyme';
-import NoteListItem from './NoteListItem';
 import { Meteor } from "meteor/meteor";
 import moment from "moment";
 
+import { notes } from "../fixtures/fixtures";
+import { NoteListItem } from './NoteListItem';
+
 
 if( Meteor.isClient ) {
-    describe('NoteListItem', function () {
 
+
+    describe('NoteListItem', function () {
         const dateFormat = 'M/DD/YY HH:mm:ss';
+        let Session;
+
+        beforeEach(() => {
+            Session = {
+                set: expect.createSpy()
+            };
+        });
 
         it('should render default title', function () {
             const title = '';
             const updatedAt = moment().valueOf();
-            const wrapper = mount(<NoteListItem note={{title, updatedAt}} />);
+            const wrapper = mount(<NoteListItem note={{ title, updatedAt }} Session={Session}/>);
             expect(wrapper.find('h5').text()).toBe('Untitled note');
             expect(wrapper.find('p').text()).toBe(moment(updatedAt).format(dateFormat));
         });
@@ -22,9 +32,16 @@ if( Meteor.isClient ) {
         it('should render title and timestamp', function () {
             const title = 'Test title';
             const updatedAt = moment().valueOf();
-            const wrapper = mount(<NoteListItem note={{title, updatedAt}} dateFormat={dateFormat} />);
+            const wrapper = mount(<NoteListItem note={{ title, updatedAt }} dateFormat={dateFormat}
+                                                Session={Session}/>);
             expect(wrapper.find('h5').text()).toBe(title);
             expect(wrapper.find('p').text()).toBe(moment(updatedAt).format(dateFormat));
         });
+
+        it('should call set on click', function () {
+            const wrapper = mount(<NoteListItem note={notes[0]} Session={Session}/>);
+            wrapper.find('div').simulate('click');
+            expect(Session.set).toHaveBeenCalledWith('selectedNoteId', notes[0]._id);
+        })
     });
 }
